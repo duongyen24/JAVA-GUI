986c21ef4ec59f0727b4df5ba29fb16c6c76d80e
duongyen <you@example.com>
2021-06-12 13:40:29 +0700

    3

--- JAVA-GUI/src/demo.java
+++ JAVA-GUI/src/demo.java
@@ -53,6 +53,45 @@ public class demo {
 		ImageIcon icon = new ImageIcon("a.jpg");
 		frame.setIconImage(icon.getImage());
 		
+
+		ImageIcon icon = new ImageIcon("a.jpg");
+		frame.setIconImage(icon.getImage());
+		Frame frame = new JFrame();
+		frame.setTitle("demo");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setSize(420,420);
+		frame.setVisible(true);
+		
+		ImageIcon icon = new ImageIcon("a.jpg");
+		frame.setIconImage(icon.getImage());
+		
+		ImageIcon icon = new ImageIcon("a.jpg");
+		frame.setIconImage(icon.getImage());
+		Frame frame = new JFrame();
+		frame.setTitle("demo");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setSize(420,420);
+		frame.setVisible(true);
+		
+		ImageIcon icon = new ImageIcon("a.jpg");
+		frame.setIconImage(icon.getImage());
+		
+		ImageIcon icon = new ImageIcon("a.jpg");
+		frame.setIconImage(icon.getImage());
+		Frame frame = new JFrame();
+		frame.setTitle("demo");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setSize(420,420);
+		frame.setVisible(true);
+		
+		ImageIcon icon = new ImageIcon("a.jpg");
+		frame.setIconImage(icon.getImage());
+		
+
+		
 	}
 
 }
